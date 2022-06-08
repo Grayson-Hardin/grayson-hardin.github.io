7bdde029eaff89da437f32db88e16ad47523a2a1
grayson-hardin <ghardin@leantechniques.com>
2022-06-08 16:23:16 -0500

    Added custom LT target image

--- main.js
+++ main.js
@@ -1,39 +1,33 @@
-import * as THREE from '../../libs/three.js-r132/build/three.module.js';
-
-document.addEventListener("DOMContentLoaded", () =>{
-    const scene = new THREE.Scene();
-
-    const geometry = new THREE.BoxGeometry(1,1,1); 
-    const material = new THREE.MeshBasicMaterial({color: "#0000FF"});
-    const cube = new THREE.Mesh(geometry, material);
-
-    scene.add(cube);
-    cube.position.set(0, 0, -2);
-    cube.rotation.set(0, Math.PI/4, 0);
-
-    const camera = new THREE.PerspectiveCamera();
-    camera.position.set(1, 1, 5);
+const THREE = window.MINDAR.IMAGE.THREE
 
 
-    const renderer = new THREE.WebGLRenderer({alpha: true});
-    renderer.setSize(500, 500);
-    renderer.render(scene, camera);
+document.addEventListener('DOMContentLoaded', () => {
+    const start = async () => {
 
 
+        const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+            container: document.body,
+            imageTargetSrc: './assets/targets/LT-mind'
+           // imageTargetSrc: './assets/targets/course-banner.mind'
 
-    const video = document.createElement("Video");
-    navigator.mediaDevices.getUserMedia({video: true}).then((stream) =>{
-        video.srcObject = stream;
-        video.play();
-    })
-
-    video.style.position = "absoulte";
-    video.style.width = renderer.domElement.width
-    video.style.height = renderer.domElement.height
-    renderer.domElement.style.position = "absolute"
-
-
-    document.body.appendChild(video)
-    document.body.appendChild(renderer.domElement)
-
+        });
+    
+        const {renderer, scene, camera} = mindarThree
+    
+        const geometry = new THREE.PlaneGeometry(1, 1);
+        const material = new THREE.MeshBasicMaterial({color: 0x0000ff, transparent: true, opacity: 0.5});
+        const plane = new THREE.Mesh(geometry, material);
+        const anchor = mindarThree.addAnchor(0);
+    
+        anchor.group.add(plane); // THREE.Group
+    
+    
+        await mindarThree.start();
+    
+        renderer.setAnimationLoop(() =>{
+            renderer.render(scene, camera);
+        });
+    }
+    start();
+ 
 });
