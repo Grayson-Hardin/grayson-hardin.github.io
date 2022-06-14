f26e7a1e3b5a61bb25b3b32cb79e163be20b45a5
grayson-hardin <ghardin@leantechniques.com>
2022-06-14 10:49:35 -0500

    added gltf import

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+ import { loadGLTF, loadAudio } from "./libs/loader.js";
 import * as THREE from './libs/three.js-r132/build/three.module.js';
 
 document.addEventListener('DOMContentLoaded', () => {
