09b528ec0e856710117b7170a0e3a3da7c0e6532
grayson-hardin <ghardin@leantechniques.com>
2022-06-16 09:58:48 -0500

    lowered scale

---
+++ script.js
@@ -0,0 +1,47 @@
+
+window.onload = () => {
+    let places = staticLoadPlaces();
+    renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+   return [
+       {
+           name: 'Magnemite',
+           location: {
+               lat: 41.15832798297468,
+               lng: -93.59179328052676,
+           }
+       },
+
+       {
+        name: 'Magnemite',
+        location: {
+            lat: 41.57369715524465,
+            lng: -93.94076137326762,
+        }
+    },
+   ];
+}
+
+function renderPlaces(places) {
+   let scene = document.querySelector('a-scene');
+
+   places.forEach((place) => {
+       let latitude = place.location.lat;
+       let longitude = place.location.lng;
+
+       let model = document.createElement('a-entity');
+       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+       model.setAttribute('gltf-model', './assets/models/magnemite/scene.gltf');
+       model.setAttribute('rotation', '0 180 0');
+       model.setAttribute('animation-mixer', '');
+       model.setAttribute('scale', '.5 .5 .5');
+
+       model.addEventListener('loaded', () => {
+           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+       });
+
+       scene.appendChild(model);
+   });
+}
