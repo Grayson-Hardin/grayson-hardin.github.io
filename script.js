bab7a2e950c21c64101465ef9972e45968c5d4da
grayson-hardin <ghardin@leantechniques.com>
2022-06-17 11:52:48 -0500

    test

--- script.js
+++ script.js
@@ -1,46 +1,65 @@
-window.onload = () => {
-    let places = staticLoadPlaces();
-    renderPlaces(places);
-};
-
-function staticLoadPlaces() {
-   return [
-       {
-           name: 'Magnemite',
-           location: {
-               lat: 41.15832798297468,
-               lng: -93.59179328052676,
-           }
-       },
-
-       {
-        name: 'Magnemite',
-        location: {
-            lat: 41.57369715524465,
-            lng: -93.94076137326762,
-        }
-    },
-   ];
-}
-
-function renderPlaces(places) {
-   let scene = document.querySelector('a-scene');
-
-   places.forEach((place) => {
-       let latitude = place.location.lat;
-       let longitude = place.location.lng;
-
-       let model = document.createElement('a-entity');
-       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-       model.setAttribute('gltf-model', './assets/models/magnemite/scene.gltf');
-       model.setAttribute('rotation', '0 180 0');
-       model.setAttribute('animation-mixer', '');
-       model.setAttribute('scale', '.5 .5 .5');
-
-       model.addEventListener('loaded', () => {
-           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-       });
-
-       scene.appendChild(model);
-   });
-}
+const loadPlaces = function(coords) {
+    // fetch data from user coords using external APIs, or simply add places data statically
+    // please look at GeoAR.js repository on examples/click-places/places.js for full code
+  }
+  
+  window.onload = () => {
+      const scene = document.querySelector('a-scene');
+  
+      // first get current user location
+      return navigator.geolocation.getCurrentPosition(function (position) {
+  
+          // than use it to load from remote APIs some places nearby
+          loadPlaces(position.coords)
+              .then((places) => {
+                  places.forEach((place) => {
+                      const latitude = place.location.lat;
+                      const longitude = place.location.lng;
+  
+                      // add place icon
+                      const icon = document.createElement('a-image');
+                      icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                      icon.setAttribute('name', place.name);
+                      icon.setAttribute('src', '../assets/map-marker.png');
+  
+                      // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
+                      icon.setAttribute('scale', '20, 20');
+  
+                      icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+  
+                      const clickListener = function(ev) {
+                          ev.stopPropagation();
+                          ev.preventDefault();
+              
+                          const name = ev.target.getAttribute('name');
+              
+                          const el = ev.detail.intersection && ev.detail.intersection.object.el;
+              
+                          if (el && el === ev.target) {
+                              const label = document.createElement('span');
+                              const container = document.createElement('div');
+                              container.setAttribute('id', 'place-label');
+                              label.innerText = name;
+                              container.appendChild(label);
+                              document.body.appendChild(container);
+              
+                              setTimeout(() => {
+                                  container.parentElement.removeChild(container);
+                              }, 1500);
+                          }
+                      };
+              
+                      icon.addEventListener('click', clickListener);
+  
+                      scene.appendChild(icon);
+                  });
+              })
+      },
+          (err) => console.error('Error in retrieving position', err),
+          {
+              enableHighAccuracy: true,
+              maximumAge: 0,
+              timeout: 27000,
+          }
+      );
+  };
