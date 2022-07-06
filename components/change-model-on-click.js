f2a1465ffb4da6b6aa29dd1815b0b67cfa312ccb
grayson-hardin <ghardin@leantechniques.com>
2022-07-06 09:19:01 -0500

    push

--- components/change-model-on-click.js
+++ components/change-model-on-click.js
@@ -34,13 +34,13 @@ AFRAME.registerComponent('change-model-on-click', {
       el.setAttribute('scale', '.5 .5 .5')
 
 
-    // if (el.getAttribute('gltf-model') === 'assets/models/flat_lady_bug/scene.gltf'){
-    //   window.setTimeout(hideSquashLadyBug, 5000);
+    if (el.getAttribute('gltf-model') === 'assets/models/flat_lady_bug/scene.gltf'){
+      window.setTimeout(hideSquashLadyBug, 5000);
 
-    //   function hideSquashLadyBug(){
-    //     el.setAttribute('visible', false)
-    //   }
-    // }
+      function hideSquashLadyBug(){
+        el.setAttribute('visible', false)
+      }
+    }
       
       function validateUserInput() {
         let userPrompt = prompt("Please enter your email: ")
